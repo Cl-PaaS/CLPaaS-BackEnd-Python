e74a9afa2fb49896c2b2098aa3af1e3ff1cd3405
hnn.ynh <hnnynh125@gmail.com>
2024-10-16 10:12:20 +0900

    fix: edit endpoint

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 logging.basicConfig(level=logging.INFO)
 
-@app.route('/check-url', methods=['POST'])
+@app.route('/', methods=['POST'])
 def check_url():
     app.logger.info('URL Checking Start')
     data = request.get_json()
